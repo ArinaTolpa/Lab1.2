8fb80a2f3c191d75bd198f2f52e298527ed738c1
ArinaTolpa <arinatolpa@gmail.com>
2024-05-24 10:36:31 +0300

    fffffffff

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,9 @@
 import unittest
 import pygame
-from main import Character, Wall, Coin, GameStats, ConfirmExit, ShowStartScreen
-from pygame.locals import QUIT, KEYDOWN, K_y, K_n
+from main import Character, Wall, Coin, LevelGenerator
 import os
+import random
+import time
 
 class TestCharacter(unittest.TestCase):
     def setUp(self):
@@ -85,101 +86,58 @@ class TestCoin(unittest.TestCase):
         self.assertEqual(coin.rect.width, 10)
         self.assertEqual(coin.rect.height, 10)
 
-class TestGameStats(unittest.TestCase):
+class TestLevelGenerator(unittest.TestCase):
+    def test_level_generator(self):
+        width, height = 5, 5
+        maze = LevelGenerator(width, height)
+        self.assertEqual(len(maze), height)
+        self.assertEqual(len(maze[0]), width)
+        self.assertEqual(maze[1][1], 0)  # Check start point is clear
+        self.assertEqual(maze[height - 2][width - 2], 0)  # Check end point is clear
+        self.assertIn(0, maze[1])  # There should be at least one path in the second row
+
+class TestGameLoop(unittest.TestCase):
     def setUp(self):
-        pygame.init()
-        self.screen_width = 800
-        self.screen_height = 600
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        self.font = pygame.font.SysFont(None, 36)
-    
-    def tearDown(self):
-        pygame.quit()
-
-    def test_game_stats_display(self):
-        message = "Test Message"
-        coin_count = 10
-        GameStats(self.screen, self.font, message, coin_count, self.screen_width, self.screen_height)
-        self.assertTrue(pygame.display.get_surface() is not None)
-    
-    def test_game_stats_content(self):
-        message = "Test Message"
-        coin_count = 10
-        GameStats(self.screen, self.font, message, coin_count, self.screen_width, self.screen_height)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.fail("GameStats caused the window to close unexpectedly")
-
-
-class TestConfirmExit(unittest.TestCase):
-    def test_confirm_exit_yes(self):
-        pygame.init()
-        screen = pygame.display.set_mode((640, 480))
-        font = pygame.font.SysFont(None, 36)
-
-        # Добавление события в очередь
-        pygame.event.post(pygame.event.Event(KEYDOWN, key=K_y))
-
-        # Проверка, что вызов ConfirmExit вызывает выход из системы
-        with self.assertRaises(SystemExit):
-            ConfirmExit(screen, font, 640, 480)
-
-    def test_confirm_exit_no(self):
-        pygame.init()
-        screen = pygame.display.set_mode((640, 480))
-        font = pygame.font.SysFont(None, 36)
-
-        # Добавление события в очередь
-        pygame.event.post(pygame.event.Event(KEYDOWN, key=K_n))
-
-        # Проверка, что ConfirmExit не завершает работу
-        try:
-            ConfirmExit(screen, font, 640, 480)
-        except SystemExit:
-            self.fail("ConfirmExit вызвал sys.exit() при нажатии 'N'")
-
-    def test_confirm_exit_quit_event(self):
-        pygame.init()
-        screen = pygame.display.set_mode((640, 480))
-        font = pygame.font.SysFont(None, 36)
-
-        # Добавление события в очередь
-        pygame.event.post(pygame.event.Event(QUIT))
-
-        # Проверка, что вызов ConfirmExit вызывает выход из системы
-        with self.assertRaises(SystemExit):
-            ConfirmExit(screen, font, 640, 480)
-
-###########################################
-class TestShowStartScreen(unittest.TestCase):
-    
-    def setUp(self):
-        # Инициализация pygame и создание экрана
         os.environ["SDL_VIDEO_CENTERED"] = "1"
         pygame.init()
         self.screen_width, self.screen_height = 740, 580
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))  # Настоящий экран
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.font = pygame.font.SysFont(None, 36)
-        
-    def test_show_start_screen(self):
-        # Симуляция нажатия клавиши ENTER для начала игры
-        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_RETURN}))
-        
-        ShowStartScreen(self.screen, self.font, self.screen_width, self.screen_height)
-        
-        # Проверка, что функция вышла из цикла while
-        self.assertTrue(True)
-    
-    def test_quit_game(self):
-        # Симуляция выхода из игры
-        pygame.event.post(pygame.event.Event(pygame.QUIT))
-        
-        with self.assertRaises(SystemExit):
-            ShowStartScreen(self.screen, self.font, self.screen_width, self.screen_height)
-        
+        self.player = Character()
+        self.walls = [Wall((50, 50)), Wall((100, 100))]
+        self.coins = [Coin((150, 150)), Coin((200, 200), negative=True)]
+        self.end_rect = pygame.Rect(300, 300, 16, 16)
+
     def tearDown(self):
         pygame.quit()
 
+    def test_player_collect_coin(self):
+        initial_coin_count = 0
+        coin_count = initial_coin_count
+
+        self.player.rect.topleft = (150, 150)
+        for coin in self.coins[:]:
+            if self.player.rect.colliderect(coin.rect):
+                self.coins.remove(coin)
+                if coin.negative:
+                    if coin_count > 0:
+                        coin_count -= 1
+                else:
+                    coin_count += 1
+
+        self.assertEqual(coin_count, initial_coin_count + 1)
+        self.assertEqual(len(self.coins), 1)
+
+    def test_player_reach_end(self):
+        self.player.rect.topleft = (300, 300)
+        self.assertTrue(self.player.rect.colliderect(self.end_rect))
+
+    def test_timer_expired(self):
+        start_time = time.time()
+        max_time = 2  # 2 seconds for quick test
+        time.sleep(3)
+        elapsed_time = time.time() - start_time
+        self.assertTrue(elapsed_time > max_time)
 
 if __name__ == '__main__':
     unittest.main()
